339e5d92fac4c82196143a5d56e751ccd951e283
Dalma <p.d.f.1915@gmail.com>
2024-11-10 03:19:10 -0300

    Se agrega funcionalidad y vistas de producto en rol vendedor

---
+++ EcommerceByteBusters/client/js/productsCrud.js
@@ -0,0 +1,25 @@
+document.getElementById('addProductForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('productName', document.getElementById('productName').value);
+    formData.append('price', document.getElementById('price').value);
+    formData.append('quanty', document.getElementById('quanty').value);
+    formData.append('img', document.getElementById('img').files[0]); // Cargar la imagen
+
+    try {
+        const response = await fetch('http://localhost:8080/clientes/add', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            alert('Producto agregado con éxito');
+        } else {
+            alert('Error al agregar el producto');
+        }
+    } catch (error) {
+        console.error('Error de conexión', error);
+    }
+});
+
