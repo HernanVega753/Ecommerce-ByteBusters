64e57ec2e4d5ce1d7a97d28b78f43569dd8d9692
Hernán Vega <hervega123@gmail.com>
2024-11-11 02:19:24 -0300

    actualización de base de datos

--- EcommerceByteBusters/server/db/dbConnections.js
+++ EcommerceByteBusters/server/db/dbConnections.js
@@ -1,12 +1,18 @@
 import mysql from "mysql2/promise";
 import dotenv from 'dotenv';
 
-dotenv.config(); // Carga las variables de .env en process.env
+dotenv.config(); // Cargar las variables de entorno
+
+// Descomponer la URL de conexión
+const dbUrl = new URL(process.env.DATABASE_URL);
 
 const pool = mysql.createPool({
-  url: process.env.DATABASE_URL, // Utiliza la URL completa proporcionada por Railway
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.substring(1), // Quitar el primer '/' de la ruta
+  port: dbUrl.port || 3306,
   connectTimeout: 60000,
 });
 
 export default pool;
-
