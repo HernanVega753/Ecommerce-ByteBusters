d6e19b51c955879ee7f4c05a5a2be1c24250390e
Hernán Vega <hervega123@gmail.com>
2024-11-11 02:14:26 -0300

    arreglado error de tipeo

--- EcommerceByteBusters/server/db/dbConnections.js
+++ EcommerceByteBusters/server/db/dbConnections.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config(); // Carga las variables de .env en process.env
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL, // Utiliza la URL completa proporcionada por Railway
+  url: process.env.DATABASE_URL, // Utiliza la URL completa proporcionada por Railway
   connectTimeout: 60000,
 });
 
