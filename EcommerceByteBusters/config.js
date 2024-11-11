4a28cd6b2a2ad235a95cb2621236d18fba002fbe
Hernán Vega <hervega123@gmail.com>
2024-11-11 01:56:57 -0300

    implementado metodo de base de datos en la nube

--- EcommerceByteBusters/config.js
+++ EcommerceByteBusters/config.js
@@ -3,23 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config(); // Carga las variables de .env en process.env
 
 export default {
-    db: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    },
+    DATABASE_URL: process.env,
     jwtSecret: process.env.JWT_SECRET,
     mercadoPagoToken: process.env.ACCESS_TOKEN,
     PORT: process.env.PORT
 };
-
-if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
-    throw new Error("Faltan variables de entorno necesarias para la base de datos.");
-}
-if (!process.env.JWT_SECRET) {
-    throw new Error("Falta la variable de entorno JWT_SECRET.");
-}
-if (!process.env.ACCESS_TOKEN) {
-    throw new Error("Falta la variable de entorno ACCESS_TOKEN.");
-}
--- EcommerceByteBusters/server/db/dbConnections.js
+++ EcommerceByteBusters/server/db/dbConnections.js
@@ -1,27 +1,11 @@
 import mysql from "mysql2/promise";
 import config from '../../config.js';
 
-// Depuración: Mostrar valores de las variables de entorno
-console.log("=== Depuración de Variables de Entorno ===");
-console.log("DB Host:", config.db.host || 'localhost');
-console.log("DB User:", config.db.user || 'root');
-console.log("DB Password:", config.db.password ? "Cargada" : "No definida"); // Oculta el valor de la contraseña
-console.log("DB Name:", config.db.database || 'constructora');
-console.log("JWT Secret:", config.jwtSecret ? "Cargada" : "No definida");
-console.log("MercadoPago Token:", config.mercadoPagoToken ? "Cargado" : "No definido");
-console.log("Puerto:", config.PORT || "No definido");
 
-console.log("=== Fin de Depuración de Variables de Entorno ===");
-
-// Crear un pool de conexiones a la base de datos usando `config`
 const pool = mysql.createPool({
-  host: config.db.host || 'localhost',
-  user: config.db.user || 'root',
-  password: config.db.password || '44057662Nicol',
-  database: config.db.database || 'constructora',
-  connectTimeout: 60000  // Aumenta el tiempo de espera a 60 segundos
+  uri: process.env.DATABASE_URL, // Utiliza la URL completa proporcionada por Railway
+  connectTimeout: 60000,
 });
 
-// Exportar el pool de conexiones
 export default pool;
 
