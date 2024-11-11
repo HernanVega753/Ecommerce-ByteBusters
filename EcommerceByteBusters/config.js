1ca621b1990fe78d5d12ba117b6939877dde430b
Hernán Vega <hervega123@gmail.com>
2024-11-11 01:06:41 -0300

    depuración

--- EcommerceByteBusters/config.js
+++ EcommerceByteBusters/config.js
@@ -12,3 +12,13 @@ export default {
     jwtSecret: process.env.JWT_SECRET,
     mercadoPagoToken: process.env.ACCESS_TOKEN
 };
+
+if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
+    throw new Error("Faltan variables de entorno necesarias para la base de datos.");
+}
+if (!process.env.JWT_SECRET) {
+    throw new Error("Falta la variable de entorno JWT_SECRET.");
+}
+if (!process.env.ACCESS_TOKEN) {
+    throw new Error("Falta la variable de entorno ACCESS_TOKEN.");
+}
